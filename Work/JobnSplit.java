2aa78c4eb73efea0f9e8be468a6bc51e1b8712fb
Something-create <sarthak.gulati@students.mq.edu.au>
2022-04-02 19:12:13 +1100

    Demo Prep Week 7

--- Work/JobnSplit.java
+++ Work/JobnSplit.java
@@ -1,14 +1,23 @@
+
 public class JobnSplit{
 
-    //types of strings
-
+    //types of outputs from sever
     String Type = new String();
+
+    //When server returns jobn
     int submitTime = 0; 
     int jobID = 0;
     int estRuntime = 0;
     int core = 0;
     int memory = 0;
     int disk = 0; 
+
+    //when server return JCPL 
+    int endTime = 0;
+    //jobID
+    String ServerType = new String();
+    int ServerID = 0;
+
 
     public JobnSplit(String s){
              
@@ -17,12 +26,21 @@ public class JobnSplit{
          sBreak = s.split(" ");
 
          Type = sBreak[0];
-         submitTime = changeSTR(sBreak[1]);
-         jobID = changeSTR(sBreak[2]);
-         estRuntime = changeSTR(sBreak[3]);
-         core = changeSTR(sBreak[4]);
-         memory = changeSTR(sBreak[5]);
-         disk = changeSTR(sBreak[6]);
+
+         if(is_JCPL()){
+            endTime = changeSTR(sBreak[1]);
+            jobID = changeSTR(sBreak[2]);
+            ServerType = sBreak[3];
+            ServerID = changeSTR(sBreak[4]);
+         }else if(is_NONE()){
+         }else{
+            submitTime = changeSTR(sBreak[1]);
+            jobID = changeSTR(sBreak[2]);
+            estRuntime = changeSTR(sBreak[3]);
+            core = changeSTR(sBreak[4]);
+            memory = changeSTR(sBreak[5]);
+            disk = changeSTR(sBreak[6]);
+         }
     }
 
     private int changeSTR(String str){
@@ -52,5 +70,18 @@ public class JobnSplit{
     }
 
 
+    public boolean is_JCPL(){
+        if(Type.equals("JCPL"))
+            return true;
+        return false;
+    }
+
+    public boolean is_NONE(){
+        if(Type.equals("NONE")){
+            return true;
+        }
+        return false;
+
+    }
 
 }
--- Work/MyClient.java
+++ Work/MyClient.java
@@ -1,242 +1,179 @@
-
 import java.io.*;
 import java.net.*;
-import java.nio.BufferOverflowException;
 
-public class MyClient {
-
+public class MyClient{
+   
    static DataOutputStream dout;
    static BufferedReader br; 
 
-   
+   static void MSG_OK(){
+      try{
+           dout.write(("OK\n").getBytes());
+           dout.flush();
+        }catch(Exception e){
+           System.out.println(e);
+        }
+     }
+  
+     static void MSG_HELO(){
+        try{
+           dout.write(("HELO\n").getBytes());
+           dout.flush();
+        }catch(Exception e){
+           System.out.println(e);
+        }
+     }
+  
+     static void MSG_AUTH(){
+        try{
+           dout.write(("AUTH Sarthak\n").getBytes());
+           dout.flush();
+        }catch(Exception e){
+           System.out.println(e);
+        }
+     }
+     static void MSG_REDY(){
+        try{
+           dout.write(("REDY\n").getBytes());
+           dout.flush();
+        }catch(Exception e){
+           System.out.println(e);
+        }
+     }
+  
+     static void MSG_GETS_C(JobnSplit job){
+        try{ 
+           int k = job.getcore();
+           dout.write(("GETS Capable "+ k + " " + job.getmemory()+ " " + job.getdisk() + " " +"\n").getBytes());
+           dout.flush();
+        }catch(Exception e){
+           System.out.println(e);
+        }
+     }
+
+     static void MSG_QUIT(){
+        try{
+         dout.write(("QUIT\n").getBytes());
+         dout.flush();
+        }catch(Exception e){
+         System.out.println(e);
+         }
+     }
+
+     static void PP(String s){
+      System.out.println("Computer Says: " + s);
+     }   
+
+
    public static void main(String[] args) {
 
       String str = new String();
-     
-      try{	
-         Socket s=new Socket("127.0.0.1",50000);
+      int serverCount = 0;
+      int big = 0;
+      int AmountServer = 0;
+      String bigID = new String();
+      Boolean CheckLargestServer = false;
+
+      try{
+
+         Socket s = new Socket("127.0.0.1",50000);
          dout = new DataOutputStream(s.getOutputStream());
          br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        // DataInputStream din = new DataInputStream(s.getInputStream());
-             
-         dout.write(("HELO\n").getBytes());
-         dout.flush();
- 
-         str = br.readLine();
-         PP(str);
 
-         dout.write(("AUTH Sarthak\n").getBytes());
-         dout.flush();
-  
-         str = br.readLine();
-         PP(str);
+         MSG_HELO();
+            str = br.readLine();
+            PP(str);
+         
+         MSG_AUTH();
+            str = br.readLine();
+            PP(str);
+         
+        String CHECKEND = new String();
 
-         dout.write(("REDY\n").getBytes());
-         dout.flush();
+         while(!(CHECKEND.equals("NONE"))){
+//when JCPL -> redy then JOBN
+            MSG_REDY();
+               str = br.readLine();
+               PP(str);  
 
-         str = br.readLine();
-         PP(str);
+            JobnSplit job; 
+            job = new JobnSplit(str);
 
-         JobnSplit job;
-         job = new JobnSplit(str);
+            while(job.is_JCPL()){
+               MSG_REDY();
+                  str = br.readLine();
+                  PP(str);
+                  job = new JobnSplit(str);  
+            }
 
-         int k = job.core;
-         dout.write(("GETS Capable "+ k + " " + job.getmemory()+ " " + job.getdisk() + " " +"\n").getBytes());
-         dout.flush();
+            if(job.is_NONE()){
+               break;
+            }
 
-         str = br.readLine();
-         PP(str);
-         DataSplit data = new DataSplit(str);
+            MSG_GETS_C(job);
+               str = br.readLine();
+               PP(str); 
+            DataSplit data;
+            data = new DataSplit(str);
 
+            MSG_OK();
 
-         dout.write(("OK\n").getBytes());
-         dout.flush();
+            JobState[] dnsJobs = new JobState[data.nRecs+1];
+            for(int i = 0; i < data.nRecs; i++){ 
+               str = br.readLine(); 
+               dnsJobs[i] = new JobState(str);
+               //System.out.println(str);
+            }  
 
-
-         //Collects all the jobs that are avaibable
-        JobState[] dnsJobs = new JobState[data.nRecs+1];
-
-         for(int i = 0; i < data.nRecs; i++){ 
-            str = br.readLine(); 
-            dnsJobs[i] = new JobState(str);
-            System.out.println(str);
-         }  
-
-         dout.write(("OK\n").getBytes());
-         dout.flush();
-
-         str = br.readLine();
-         PP(str);
-
-       // Search algorithm to find the largest
-      
-       /*
-       try to find the server with the largest core
-       then store the the serverID and the core amount 
-       And the amount of servers that are there then keep
-       sending jobs until the server count is meet then send
-       them back to 0 and countiue untill it doesnt have more 
-       jobs. 
-
-       search algorithm 
-       1. Find the largest core
-         When they both are the same then choose the first one 
-            also get the server type
-            
-       2. Then with another loop find the amount of servers with that type
-        
-
-       it will be checked with java 1.8
-       next week demo
-       javac -source 1.8 _target 1.8 *.java
-      */ 
-
-      
-int big = 0;
-int ao = 0;
-String bigID = new String();
-
-for(int i = 1; i < data.nRecs;i++){
-   for(int x = 0; x < data.nRecs; x++){
-      if(dnsJobs[i].core == dnsJobs[x].core){
-         if(big == dnsJobs[i].core){ao++;}
-      }else {//if(dnsJobs[i].core > dnsJobs[x].core){
-         big = dnsJobs[i].core;
-         bigID = dnsJobs[i].serverType;
-         ao = 0;
-      }
-   }
-}
-
-         System.out.println(bigID + " "+ ao + " " + big );
-         for(int i = 0; i < ao; i++){
-            dout.write(("SCHD "+ job.jobID + " "+ bigID + " " + i +"\n").getBytes());
-            dout.flush();
+            if(!CheckLargestServer){
+            CheckLargestServer = true;
+            for(int i = 0; i < data.nRecs;i++){
+                  for(int x = 0; x < data.nRecs; x++){
+                     if(dnsJobs[i].core == dnsJobs[x].core){
+                        if(big == dnsJobs[i].core){AmountServer++;}
+                     }else {
+                        big = dnsJobs[i].core;
+                        bigID = dnsJobs[i].serverType;
+                        AmountServer = 0;
+                     }
+                  }
+              }
+            System.out.println("**Server Type- "+bigID + " **Amount of Servers-  "+ AmountServer + " **Amount of cores- " + big );
 
          }
 
-         str = br.readLine();
-         PP(str);
+
+            MSG_OK();
+                str = br.readLine();
+                PP(str); 
+
+            dout.write(("SCHD "+ job.jobID + " "+ bigID + " " + serverCount +"\n").getBytes());
+            dout.flush();
+            serverCount++;
+                str = br.readLine();
+                PP(str); 
+
+            MSG_OK();
+                str = br.readLine();
+                PP(str); 
+
+            if(serverCount > AmountServer ){
+              serverCount = 0;
+            }
+           CHECKEND = br.readLine();
+        }
+
+         MSG_QUIT();
+            str = br.readLine();
+            PP(str);
 
          dout.close();
          s.close();   
-      
+
       }catch(Exception e){
-         System.out.println(e);
+        System.out.println(e);
       }
 
-   }  
-  
-   static void PP(String s){
-      System.out.println("Computer Says: " + s);
-   }
-
-   static void MSG_ok(){
-      dout.write(("OK\n").getBytes());
-      dout.flush();
    }
 
 }
-
-
-
-/*
-
-int big = 0;
-int ao = 0;
-String bigID = new String();
-Boolean biggest = false;
-
-
-for(int i = 1; i < data.nRecs;i++){
-   for(int x = 0; x < data.nRecs; x++){
-      if(dnsJobs[i].core == dnsJobs[x].core){
-         if((dnsJobs[i].serverType).equals(dnsJobs[x].serverType)){
-            ao++;
-         }else{
-            ao = 0;
-         }
-      }else if(dnsJobs[i].core < dnsJobs[x].core){
-          bigID = dnsJobs[x].serverType;
-          biggest = true;
-          big = dnsJobs[x].core;  
-      }else{
-         ao = 0;
-      }
-   }
-   if(biggest)break;
-
-}
-*/
-
-
-
-
-
-   //dout.write(("QUIT\n").getBytes());
-
-
-//while(!str.equals("BYE\n")){
-
-//}
-
-/**
- * 
- *  dout.writeUTF("HELO");
-    dout.flush();
-    str2 = din.readUTF();
-    System.out.println("Server says: "+ str2);
-    //str = "BYE";
-    dout.writeUTF("BYE");
-    dout.flush();
-    str2 = din.readUTF();
-    System.out.println("Server says: "+ str2);
- */
-
-/**
- *  // str = "HELO";
-    dout.write(("HELO/n").getBytes());
-    dout.flush();
-    str2 = br.readLine();
-    System.out.println("Server says: "+ str2);
-    //str = "BYE";
-    dout.write(("BYE/n").getBytes());
-    dout.flush();
-    str2 = br.readLine();
-    System.out.println("Server says: "+ str2);
- */
-
-/**
- * 
- * while(!str.equals("BYE")){
-    str = br.readLine();
-    dout.writeUTF(str);
-    dout.flush();
-    str2 = din.readUTF();
-    System.out.println("Server says: "+ str2);
-}
- * 
- */
-
-
-//dout.writeUTF("Hello Server");
-//dout.flush();
-
-
-
-   // dout.write(("REDY\n").getBytes());
-   // dout.flush();
-
-   // str = br.readLine();
-   // //System.out.println(str + "\n");
-   
-   // dout.write(("REDY\n").getBytes());
-   // dout.flush();
-   
-
-   
-   //dout.write(("GETS Capable\n" + ).getBytes());
-   //dout.flush();
-
-   
-   //System.out.println(str2+ "\n");
