57554df6b379861119fbb2c92a634acae3e3f543
Something-create <sarthak.gulati@students.mq.edu.au>
2022-05-25 16:47:01 +1000

    .

--- Stage_2/assignServer.java
+++ Stage_2/assignServer.java
@@ -12,55 +12,56 @@ public class assignServer {
 
     public assignServer(JobState[] js){
         servers = js;
-        findServerAmount();
+     //  findServerAmount();
     }
 
-    public void findServerAmount(){
-        String Current = servers[0].serverType;
-        Integer amount = 0;
-        for(int i = 0; i <= servers.length; i++){
-                if(Current.equals(servers[i].serverType)){
-                    amount++;
-                }else{
-                    addToSearched(Current, amount);
-                    Current = servers[i].serverType;
-                    amount = 0;
-                }
-        }
-    }
+    // public void findServerAmount(){
+    //     String Current = servers[0].serverType;
+    //     Integer amount = 0;
+    //     for(int i = 0; i <= servers.length; i++){
+    //             if(Current.equals(servers[i].serverType)){
+    //                 amount++;
+    //             }else{
+    //                 addToSearched(Current, amount);
+    //                 Current = servers[i].serverType;
+    //                 amount = 0;
+    //             }
+    //     }
+    // }
 
-        //add the amount of servers to a hashmap 
-    public void addToSearched(String s, Integer i){
-        serverAmount.put(s, i);
-    }
+    //     //add the amount of servers to a hashmap 
+    // public void addToSearched(String s, Integer i){
+    //     serverAmount.put(s, i);
+    // }
     
-    public int alreadySearched(String s){   
-        if(serverAmount.get(s) == null)
-            return 0;
-        return serverAmount.get(s);
-    } 
+    // public int alreadySearched(String s){   
+    //     if(serverAmount.get(s) == null)
+    //         return 0;
+    //     return serverAmount.get(s);
+    // } 
 
-    public int currentCount(String s){
-        return 0;
-    }
+    // public int currentCount(String s){
+    //     return 0;
+    // }
 
-    public String WhichServer(String s){
+    // public String WhichServer(String s){
 
-        if(serverAmount.get(s) == null){
-            curPos.put(s, 1);
-        }
-        int cur = curPos.get(s);
-        curPos.replace(s, cur, cur++);
-        return servers[0].serverType;
-    }
+    //     if(serverAmount.get(s) == null){
+    //         curPos.put(s, 1);
+    //     }
+    //     int cur = curPos.get(s);
+    //     curPos.replace(s, cur, cur++);
+    //     return servers[0].serverType;
+    // }
     
-    static void print(Object o){
-        System.out.println(o);
-     }
+    // static void print(Object o){
+    //     System.out.println(o);
+    //  }
 
     void MSG_SCHD(int i){
         try{
-            dout.write(("SCHD "+ i + " "+  WhichServer(servers[0].serverType) + " " + curPos.get(servers[0].serverType) +"\n").getBytes());
+            //dout.write(("SCHD "+ i + " "+  WhichServer(servers[0].serverType) + " " + curPos.get(servers[0].serverType) +"\n").getBytes());
+            dout.write(("SCHD "+ i + " "+  servers[0].serverType + " " + 1 +"\n").getBytes());
             dout.flush();
         }catch(Exception e){
          System.out.println(e);
